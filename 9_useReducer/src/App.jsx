0cf6cc874ec4476666ddcc3e206a8dbd45e3b5a6
ridvankeskin <teso_ridvan@hotmail.com>
2024-03-25 16:32:47 +0300

    update

---
+++ 9_useReducer/src/App.jsx
@@ -0,0 +1,11 @@
+import Counter from "./components/Counter";
+
+const App = () => {
+  return (
+    <div className="vh-100 d-flex justify-content-center align-items-center">
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
---
+++ 9_useReducer/src/components/Counter.jsx
@@ -0,0 +1,12 @@
+const Counter = () => {
+  return (
+    <div>
+      <button>Azalt</button>
+      <span>0</span>
+      <button>Arttır</button>
+
+    </div>
+  );
+};
+
+export default Counter;
